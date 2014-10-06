ea0c0795b00264dce68ed2adc8208ccd55cbf117
Michael J. Petruzzello <tedybear3@aol.com>
2014-10-06 13:48:40 -0400

    Adds GUI.
    The original program.

---
+++ IRSAway/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace IRSAway
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewAccounts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMonth = new System.Windows.Forms.TextBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ColumnDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTransaction = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnSalesTax = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnExempt = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAccounts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(646, 24);
+            this.menuStripMain.TabIndex = 0;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemSave});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // toolStripMenuItemSave
+            // 
+            this.toolStripMenuItemSave.Name = "toolStripMenuItemSave";
+            this.toolStripMenuItemSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuItemSave.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItemSave.Text = "&Save";
+            this.toolStripMenuItemSave.Click += new System.EventHandler(this.toolStripMenuItemSave_Click);
+            // 
+            // dataGridViewAccounts
+            // 
+            this.dataGridViewAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAccounts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnDate,
+            this.ColumnTransaction,
+            this.ColumnAmount,
+            this.ColumnNet,
+            this.ColumnSalesTax,
+            this.ColumnExempt});
+            this.dataGridViewAccounts.Location = new System.Drawing.Point(15, 135);
+            this.dataGridViewAccounts.Name = "dataGridViewAccounts";
+            this.dataGridViewAccounts.Size = new System.Drawing.Size(618, 297);
+            this.dataGridViewAccounts.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Month:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Year:";
+            // 
+            // textBoxMonth
+            // 
+            this.textBoxMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMonth.Location = new System.Drawing.Point(120, 38);
+            this.textBoxMonth.Name = "textBoxMonth";
+            this.textBoxMonth.ReadOnly = true;
+            this.textBoxMonth.Size = new System.Drawing.Size(87, 23);
+            this.textBoxMonth.TabIndex = 4;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxYear.Location = new System.Drawing.Point(120, 64);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.ReadOnly = true;
+            this.textBoxYear.Size = new System.Drawing.Size(87, 23);
+            this.textBoxYear.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(213, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Change Date";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ColumnDate
+            // 
+            this.ColumnDate.HeaderText = "Date";
+            this.ColumnDate.MaxInputLength = 2;
+            this.ColumnDate.Name = "ColumnDate";
+            this.ColumnDate.Width = 50;
+            // 
+            // ColumnTransaction
+            // 
+            this.ColumnTransaction.HeaderText = "Transaction";
+            this.ColumnTransaction.MaxInputLength = 40;
+            this.ColumnTransaction.Name = "ColumnTransaction";
+            this.ColumnTransaction.Width = 125;
+            // 
+            // ColumnAmount
+            // 
+            this.ColumnAmount.HeaderText = "Amount";
+            this.ColumnAmount.MaxInputLength = 10;
+            this.ColumnAmount.Name = "ColumnAmount";
+            // 
+            // ColumnNet
+            // 
+            this.ColumnNet.HeaderText = "Net";
+            this.ColumnNet.MaxInputLength = 10;
+            this.ColumnNet.Name = "ColumnNet";
+            // 
+            // ColumnSalesTax
+            // 
+            this.ColumnSalesTax.HeaderText = "Sales Tax";
+            this.ColumnSalesTax.MaxInputLength = 10;
+            this.ColumnSalesTax.Name = "ColumnSalesTax";
+            // 
+            // ColumnExempt
+            // 
+            this.ColumnExempt.HeaderText = "Exempt";
+            this.ColumnExempt.Name = "ColumnExempt";
+            this.ColumnExempt.Width = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sales Tax:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(92, 93);
+            this.textBox3.MaxLength = 2;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(22, 23);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "7";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 444);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.textBoxMonth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewAccounts);
+            this.Controls.Add(this.menuStripMain);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStripMain;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "IRSAway";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAccounts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSave;
+        private System.Windows.Forms.DataGridView dataGridViewAccounts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMonth;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTransaction;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnSalesTax;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnExempt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ IRSAway/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IRSAway
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItemSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("Accounts\\" + textBoxYear.Text + "\\" + textBoxMonth.Text + ".txt");
+            int indexSize = dataGridViewAccounts.Rows.Count;
+            string[] dates = new string[indexSize], transactions = new string[indexSize], amounts = new string[indexSize],
+                nets = new string[indexSize], salestaxes = new string[indexSize];
+
+            //CODE A CHECK TO MAKE SURE NO ROW HAS ANY EMPTY SPOTS.
+
+            foreach(DataGridViewRow row in dataGridViewAccounts.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    if (row.Cells[i].ColumnIndex == 0)
+                        dates[i] = (string) row.Cells[i].Value;
+                }
+            }
+        }
+    }
+}
