e2ccb0fd1ac90983fcdfd1f9700754790c75635b
Michael Petruzzello <Tedybear3@aol.com>
2014-10-06 14:34:53 -0400

    Creates database and updates GUI.

--- IRSAway/Form1.cs
+++ IRSAway/Form1.cs
@@ -13,21 +13,21 @@ namespace IRSAway
 
         private void toolStripMenuItemSave_Click(object sender, EventArgs e)
         {
-            StreamWriter writer = new StreamWriter("Accounts\\" + textBoxYear.Text + "\\" + textBoxMonth.Text + ".txt");
-            int indexSize = dataGridViewAccounts.Rows.Count;
-            string[] dates = new string[indexSize], transactions = new string[indexSize], amounts = new string[indexSize],
-                nets = new string[indexSize], salestaxes = new string[indexSize];
+            //StreamWriter writer = new StreamWriter("Accounts\\" + textBoxYear.Text + "\\" + textBoxMonth.Text + ".txt");
+            //int indexSize = dataGridViewAccounts.Rows.Count;
+            //string[] dates = new string[indexSize], transactions = new string[indexSize], amounts = new string[indexSize],
+            //    nets = new string[indexSize], salestaxes = new string[indexSize];
 
-            //CODE A CHECK TO MAKE SURE NO ROW HAS ANY EMPTY SPOTS.
+            ////CODE A CHECK TO MAKE SURE NO ROW HAS ANY EMPTY SPOTS.
 
-            foreach(DataGridViewRow row in dataGridViewAccounts.Rows)
-            {
-                for (int i = 0; i < row.Cells.Count; i++)
-                {
-                    if (row.Cells[i].ColumnIndex == 0)
-                        dates[i] = (string) row.Cells[i].Value;
-                }
-            }
+            //foreach(DataGridViewRow row in dataGridViewAccounts.Rows)
+            //{
+            //    for (int i = 0; i < row.Cells.Count; i++)
+            //    {
+            //        if (row.Cells[i].ColumnIndex == 0)
+            //            dates[i] = (string) row.Cells[i].Value;
+            //    }
+            //}
         }
     }
 }
